c338f4e74fb2ca0642564635bd78ec042b1f440f
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-22 11:34:06 +0200

    Implement Profile DTO

--- ChatService.Web/Dtos/Profile.cs
+++ ChatService.Web/Dtos/Profile.cs
@@ -1,3 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ChatService.Web.Dtos;
 
-public record Profile();
+public record Profile(
+    [Required] string Username,
+    [Required] string FirstName,
+    [Required] string LastName,
+    [Required] string ProfilePictureId
+    );
