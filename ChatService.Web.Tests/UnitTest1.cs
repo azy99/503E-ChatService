83ab8e7a2ee0efc4a6ee28b25f194b224ee9559c
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-22 10:31:16 +0200

    Initialize project

---
+++ ChatService.Web.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace ChatService.Web.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ ChatService.Web/Controllers/ProfileController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatService.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProfileController : ControllerBase
+{
+    
+}
