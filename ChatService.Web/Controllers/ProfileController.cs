3dd67dab9a66207d7b215b2293a5a47c094a6b2b
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-22 11:36:28 +0200

    Implement Profile Controller

--- ChatService.Web/Controllers/ProfileController.cs
+++ ChatService.Web/Controllers/ProfileController.cs
@@ -1,3 +1,5 @@
+using ChatService.Web.Dtos;
+using ChatService.Web.Storage;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatService.Web.Controllers;
@@ -6,5 +8,36 @@ namespace ChatService.Web.Controllers;
 [Route("[controller]")]
 public class ProfileController : ControllerBase
 {
+    private readonly IProfileStore _profileStore;
+    
+    public ProfileController(IProfileStore profileStore)
+    {
+        _profileStore = profileStore;
+    }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<Profile>> GetProfile(string username)
+    {
+        var profile = await _profileStore.GetProfile(username);
+        if (profile == null)
+        {
+            return NotFound($"A user with {username} was not found");
+        }
+
+        return Ok(profile);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Profile>> AddProfile(Profile profile)
+    {
+        var existingProfile = await _profileStore.GetProfile(profile.Username);
+        if (existingProfile != null)
+        {
+            return Conflict($"A user with username {profile.Username} already exists");
+        }
+
+        await _profileStore.UpsertProfile(profile);
+        return CreatedAtAction(nameof(GetProfile), new {username = profile.Username}, profile);
+    }
     
 }
