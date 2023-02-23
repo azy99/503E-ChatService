8b18995f8e6e4250d6611b0cc3b291cb6b520e66
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-23 04:37:13 +0200

    Update IFileStore

--- ChatService.Web/Storage/IFileStore.cs
+++ ChatService.Web/Storage/IFileStore.cs
@@ -6,6 +6,6 @@ namespace ChatService.Web.Storage;
 public interface IFileStore
 {
     Task<UploadImageResponse?> UploadFile(UploadImageRequest file);
-    Task<Stream?> DownloadFile(string fileId);
+    Task<BlobResponse> DownloadFile(string fileId);
     Task DeleteFile(string fileId);
 }
