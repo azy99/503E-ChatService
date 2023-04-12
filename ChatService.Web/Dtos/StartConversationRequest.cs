aa1ea11330b68027c1c9cd34c45fccf04eb7f5c3
NadimChibani <npc00@mail.aub.edu>
2023-04-12 14:22:10 +0300

    Implement StartConversation Request and Response DTO

---
+++ ChatService.Web/Dtos/StartConversationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ChatService.Web.ValidationAttributes;
+
+namespace ChatService.Web.Dtos
+{
+    public record StartConversationRequest(
+        [Required] [MinimumCountAttribute(2)] List <string> Participants,
+        [Required] Message FirstMessage
+        );
+    
+}
---
+++ ChatService.Web/Dtos/StartConversationResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatService.Web.Dtos
+{
+    public record StartConversationResponse(
+        [Required] string conversationId,
+        [Required] long CreatedUnixTime
+);
+}
