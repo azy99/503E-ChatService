2a25757a7ed82be047760a81193198e9c50c578b
NadimChibani <npc00@mail.aub.edu>
2023-04-12 14:22:54 +0300

    Implement custom list size attribute

---
+++ ChatService.Web/CustomValidation/MinimumCountAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatService.Web.ValidationAttributes
+{
+    public class MinimumCountAttribute : ValidationAttribute
+    {
+        private readonly int _minimumCount;
+
+        public MinimumCountAttribute(int minimumCount)
+        {
+            _minimumCount = minimumCount;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is not ICollection collection || collection.Count < _minimumCount)
+            {
+                return new ValidationResult($"The '{validationContext.DisplayName}' field must have at least {_minimumCount} items.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
