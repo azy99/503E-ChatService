2adaa7f1f3e4959cced6ae53692f1d020aee219b
NadimChibani <npc00@mail.aub.edu>
2023-04-12 12:39:56 +0300

    Implement userConversation DTO

---
+++ ChatService.Web/Dtos/userConversation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatService.Web.Dtos
+{
+    public record userConversation(
+        [Required] string Sender,
+        [Required] string Receiver,
+        [Required] Message FirstMessage
+        );
+
+}
