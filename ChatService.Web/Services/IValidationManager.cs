e63bf4fff8faa5c65a100a7fea6f7538fe0e207a
NadimChibani <npc00@mail.aub.edu>
2023-04-15 13:53:47 +0300

    Implement Validation Manager to keep validations used for both messages and conversations in one place

---
+++ ChatService.Web/Services/IValidationManager.cs
@@ -0,0 +1,14 @@
+﻿using ChatService.Web.Dtos.Conversations;
+using ChatService.Web.Dtos.Messages;
+using ChatService.Web.Dtos.Profiles;
+
+namespace ChatService.Web.Services
+{
+    public interface IValidationManager
+    {
+        public void ValidateConversation(StartConversationRequest request);
+        public void ValidateMessage(Message message, bool IsFirstMessage);
+        public void CheckIfSenderExists(string senderUsername);
+        public void CheckIfReceiverExists(string receiverUsername);
+    }
+}
---
+++ ChatService.Web/Services/ValidationManager.cs
@@ -0,0 +1,69 @@
+﻿using ChatService.Web.Dtos.Conversations;
+using ChatService.Web.Dtos.Messages;
+using ChatService.Web.Dtos.Profiles;
+using ChatService.Web.Exceptions;
+using ChatService.Web.Storage;
+
+namespace ChatService.Web.Services
+{
+    public class ValidationManager: IValidationManager
+    {
+        private readonly IConversationStore _conversationStore;
+        private readonly IProfileStore _profileStore;
+        public ValidationManager(IConversationStore conversationStore, IProfileStore profileStore)
+        {
+            _conversationStore = conversationStore;
+            _profileStore = profileStore;
+        }
+        public void ValidateConversation(StartConversationRequest request)
+        {
+            if (request == null)
+            {
+                throw new NullStartConversationRequestException(nameof(request));
+            }
+
+            if (request.Participants.Count < 2 || request.Participants.Count > 2 || request.Participants == null)
+            {
+                throw new ConversationNotTwoPeople();
+            }
+            CheckIfSenderExists(request.Participants[0]);
+            CheckIfReceiverExists(request.Participants[1]);
+
+            ValidateMessage(request.FirstMessage,true);
+
+        }
+        public void ValidateMessage(Message message,bool IsFirstMessage)
+        {
+            if (message == null)
+            {
+                throw new NullMessage();
+            }
+            if (string.IsNullOrEmpty(message.Id) ||
+               string.IsNullOrEmpty(message.SenderUsername) ||
+               string.IsNullOrEmpty(message.Text))
+            {
+                throw new InvalidMessageParams();
+            }
+            if (!IsFirstMessage)
+            {
+                CheckIfSenderExists(message.SenderUsername);
+            }
+        }
+        public void CheckIfSenderExists(string senderUsername)
+        {
+            var sender = _profileStore.GetProfile(senderUsername);
+            if (sender == null)
+            {
+                throw new SenderDoesNotExist(senderUsername);
+            }
+        }
+        public void CheckIfReceiverExists(string receiverUsername)
+        {
+            var recipient = _profileStore.GetProfile(receiverUsername);
+            if (recipient == null)
+            {
+                throw new ReceiverDoesNotExist(receiverUsername);
+            }
+        }
+    }
+}
