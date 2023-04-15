e2cc5f890ef65b93585e9fdd7a6cf1eef22deffd
NadimChibani <npc00@mail.aub.edu>
2023-04-15 14:16:10 +0300

    Refactore IMessageStore

---
+++ ChatService.Web/Storage/IMessageStore.cs
@@ -0,0 +1,10 @@
+﻿using ChatService.Web.Dtos.Messages;
+
+namespace ChatService.Web.Storage
+{
+    public interface IMessageStore
+    {
+        public Task<UserMessage> AddMessage(UserMessage message);
+        public Task<UserMessage?> GetMessage(string messageID);
+    }
+}
