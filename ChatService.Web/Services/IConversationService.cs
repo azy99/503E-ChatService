1c1b118707ddca97c5831a07cdd60b73fd505f48
NadimChibani <npc00@mail.aub.edu>
2023-04-15 08:39:05 +0300

    Implement service interface

---
+++ ChatService.Web/Services/IConversationService.cs
@@ -0,0 +1,11 @@
+﻿using ChatService.Web.Dtos;
+
+namespace ChatService.Web.Services
+{
+    public interface IConversationService
+    {
+        Task <StartConversationResponse> CreateConversation(StartConversationRequest request);
+        Task<userConversation?> GetConversation(string conversationID);
+        //Task UpdateConversation(userConversation conversation);
+    }
+}
