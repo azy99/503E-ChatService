6cdc0c20e763fb5f2f542c23c090b8929b01466f
NadimChibani <npc00@mail.aub.edu>
2023-04-15 08:39:14 +0300

    Implement Service

---
+++ ChatService.Web/Services/ConversationService.cs
@@ -0,0 +1,29 @@
+﻿using ChatService.Web.Dtos;
+using ChatService.Web.Storage;
+
+namespace ChatService.Web.Services
+{
+    //TODO Different DTOs between layers: controller -> service -> storage to add logic in service layer
+    public class ConversationService : IConversationService
+    {
+        private readonly IConversationStore _conversationStore;
+        public ConversationService(IConversationStore conversationStore)
+        {
+            _conversationStore = conversationStore;
+        }
+
+        public Task<StartConversationResponse> CreateConversation(StartConversationRequest request)
+            //TODO CALL THE AddConversation on both sides
+            //TODO Add Validations here
+            //TODO Add toEntitie in service
+        {
+            //create current time, add it to request and ccall _conversationStore.UpdateConversation(request)
+            return _conversationStore.AddConversation(request);
+        }
+
+        public Task<userConversation?> GetConversation(string conversationID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
