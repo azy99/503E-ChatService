88beae124ecca61f122a2d60ca443d48cef6ae34
NadimChibani <npc00@mail.aub.edu>
2023-04-15 08:41:43 +0300

    Implement conversation store interface

---
+++ ChatService.Web/Storage/IConversationStore.cs
@@ -0,0 +1,11 @@
+﻿using ChatService.Web.Dtos;
+
+namespace ChatService.Web.Storage
+{
+    public interface IConversationStore
+    {
+        Task UpsertConversation(StartConversationRequest conversation);
+        Task<StartConversationResponse?> GetConversation(string conversationID);
+        Task<StartConversationResponse> AddConversation(StartConversationRequest request);
+    }
+}
