e0a50b823db133beb4bd3547a9240f8d5bafb2be
NadimChibani <npc00@mail.aub.edu>
2023-04-15 08:41:56 +0300

    Implement Cosmos Conversation Store

---
+++ ChatService.Web/Storage/CosmosConversationStore.cs
@@ -0,0 +1,116 @@
+﻿using ChatService.Web.Configuration;
+using ChatService.Web.Dtos;
+using ChatService.Web.Exceptions;
+using ChatService.Web.Storage.Entities;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Options;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ChatService.Web.Storage
+{
+    public class CosmosConversationStore : IConversationStore
+    {
+        private readonly CosmosClient _cosmosClient;
+        public CosmosConversationStore(IOptions<CosmosSettings> options)
+        {
+            _cosmosClient = new CosmosClient(options.Value.ConnectionString);
+        }
+
+        private Container Container => _cosmosClient.GetDatabase("profiles").GetContainer("sharedContainer");
+
+        public async Task<StartConversationResponse> AddConversation(StartConversationRequest request)
+        {
+            var entity = ToEntity(request);
+            try
+            {
+                ValidateConversation(request);
+                await Container.CreateItemAsync(entity);
+            }
+            catch (CosmosException e)
+            {
+                if (e.StatusCode == HttpStatusCode.Conflict)
+                {
+                    //Should you return this or call get and return what you get?
+                    return await GetConversation(entity.Id);
+                }
+
+                throw;
+            }
+            return ToStartConversationResponse(entity);
+        }
+
+        public async Task<StartConversationResponse?> GetConversation(string conversationID)
+        {
+            try
+            {
+                var entity = await Container.ReadItemAsync<ConversationEntity>(
+                    id: conversationID,
+                    partitionKey: new PartitionKey(conversationID)
+                );
+                return ToStartConversationResponse(entity);
+            }
+            catch (CosmosException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                throw;
+            }
+        }
+
+        public Task UpsertConversation(StartConversationRequest conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateConversation(StartConversationRequest request)
+        {
+            if (request == null)
+            {
+                throw new NullStartConversationRequestException(nameof(request));
+            }
+
+            if (request.Participants.Count < 2 || request.Participants.Count > 2 || request.Participants == null)
+            {
+                throw new ConversationNotTwoPeople();
+            }
+
+            ValidateMessage(request.FirstMessage);
+        }
+        public void ValidateMessage(Message message)
+        {
+            //But required validation in DTO should deal with that
+
+            //Only left to check if sender exists
+            if (message == null)
+            {
+                throw new NullMessage();
+            }
+            if(string.IsNullOrEmpty(message.Id)||
+               string.IsNullOrEmpty(message.SenderUsername)||
+               string.IsNullOrEmpty(message.Text))
+            {
+                throw new InvalidMessageParams();
+            }
+
+        }
+        private static ConversationEntity ToEntity(StartConversationRequest request)   
+        {
+            return new ConversationEntity(
+                partitionKey: request.Participants[0],
+                Id: request.Participants[0] + "_" + request.Participants[1],
+                LastModifiedUnixTime: DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                ReceiverUsername: request.Participants[1]
+            );
+        }
+        private static StartConversationResponse ToStartConversationResponse(ConversationEntity entity)
+        {
+            return new StartConversationResponse(
+                ConversationId: entity.Id,
+                CreatedUnixTime: entity.LastModifiedUnixTime
+            );
+        }
+    }
+}
