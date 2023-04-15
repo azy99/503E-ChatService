1ed6696a32c5baac497b21bbfb5fb5b88019ed8c
NadimChibani <npc00@mail.aub.edu>
2023-04-15 08:40:44 +0300

    Implement conversation entity

---
+++ ChatService.Web/Storage/Entities/ConversationEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatService.Web.Storage.Entities
+{
+    public record ConversationEntity(
+        string partitionKey,        //Username
+        string Id,                  // ConversationId: SenderUsername_ReceiverUsername  split based on _ to retrieve sender, receiver
+        long LastModifiedUnixTime,
+        string ReceiverUsername     //TODO INSTEAD OF ONLY RECEIVERUSERNAME, MIGHT AS WELL GET THE PROFILE AND SAVE ITS
+        );
+    
+}
