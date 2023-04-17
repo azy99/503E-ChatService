b53fe194f62aee87c8ee7788c45f360429fe9687
NadimChibani <npc00@mail.aub.edu>
2023-04-18 00:16:11 +0300

    Fixing for cosmos parameters

--- ChatService.Web/Storage/Entities/MessageEntity.cs
+++ ChatService.Web/Storage/Entities/MessageEntity.cs
@@ -4,7 +4,7 @@ namespace ChatService.Web.Storage.Entities
 {
     public record MessageEntity(
         [Required] string partitionKey,    //ConversationId
-        [Required] string Id,              //messageId
+        [Required] string id,              //messageId
         [Required] string Text,
         [Required] string SenderUsername,
         [Required] long UnixTime
