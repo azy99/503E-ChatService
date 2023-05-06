a6cbeac7fe58e6290742d29a049e49251629b699
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-05-06 20:54:14 +0300

    Update Image and Profile endpoints

--- ChatService.Web/Controllers/ImagesController.cs
+++ ChatService.Web/Controllers/ImagesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ChatService.Web.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ImagesController : ControllerBase
 {
     private readonly IFileStore _fileStore;
